9fab1080360ca56d7aaf5a89ef0ce7ef52f2826f
Jie Liang <jieliang456@gmail.com>
2018-05-11 22:05:08 -0400

    add cookie parser to the service

--- backend/follow-service/app.js
+++ backend/follow-service/app.js
@@ -1,7 +1,9 @@
 const express   = require('express');
 const app       = express();
+const cookieParser = require('cookie-parser');
 const router    = require('./routes/routes');
 
+app.use(cookieParser());
 app.use('/', router);
 
 app.listen(3003);
